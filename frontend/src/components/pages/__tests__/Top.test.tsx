de8cbee0d96e1388add892ec7b973912470ba1c1
中畠昇汰 <pandora.osk@gmail.com>
2024-09-28 02:18:28 +0800

    add Top.page test2

--- frontend/src/components/pages/__tests__/Top.test.tsx
+++ frontend/src/components/pages/__tests__/Top.test.tsx
@@ -4,15 +4,25 @@ import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Top from "../Top"
 
-describe("Top Page",()=>{
-    test('renders the main title',()=>{
+describe("Top Page", () => {
+    beforeEach(() => {
         render(
             <Router>
-                <Top/>
+                <Top />
             </Router>
         );
+    })
+    test('renders the main title', () => {
         // メインタイトルが正しく表示されているかを確認
         const mainTitle = screen.getByText(/Where is My Restroom\?\?/i);
         expect(mainTitle).toBeInTheDocument();
-    })
+    });
+
+    test('renders the explanation paragraphs',()=>{
+        // 説明文が正しく表示されているかを確認
+        const firstParagraph = screen.getByText(/This website allows you to easily find nearby Restrooms/i);
+        const secondParagraph = screen.getByText(/Accessible options are also available/i);
+        expect(firstParagraph).toBeInTheDocument()
+        expect(secondParagraph).toBeInTheDocument()
+    });
 })
